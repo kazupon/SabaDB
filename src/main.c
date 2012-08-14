a5367a2ea40ffbc6737ba0e8f611f1e6b672476c
kazuya kawaguchi <kawakazu80@gmail.com>
2012-08-14 23:40:27 +0900

    update sabadb main function.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,44 @@
 /*
- * main
+ * SabaDB: main etnry point
  * Copyright (C) 2012 kazuya kawaguchi & frapwings. See Copyright Notice in main.h
  */
 
 #include "main.h"
 #include "debug.h"
-#include <stdio.h>
+#include "saba_utils.h"
+#include "saba_common.h"
+#include "saba_server.h"
+#include "saba_worker.h"
+#include <assert.h>
 #include <uv.h>
-#include <kclangc.h>
 
+
+static saba_server_t *server;
+
+static void on_signal(int signo) {
+  TRACE("signo: %d\n", signo);
+
+  if (server) {
+    saba_err_t err = saba_server_stop(server);
+    TRACE("stop server: err=%d\n", err);
+  }
+}
 
 int main () {
+  signal(SIGINT, &on_signal);
+  signal(SIGTERM, &on_signal);
+  signal(SIGPIPE, SIG_IGN);
+
   uv_loop_t *loop = uv_default_loop();
-  TRACE("hello libuv\n");
-  KCDB *db = kcdbnew();
-  TRACE("hello kyotocabinet for c lang\n");
-  kcdbdel(db);
-  uv_run(loop);
-  return 0;
+
+  server = saba_server_alloc();
+  assert(server != NULL);
+
+  int32_t ret = saba_server_start(server, loop, "0.0.0.0", 1978);
+  TRACE("server start: ret=%d\n", ret);
+
+  ret = uv_run(loop);
+  saba_server_free(server);
+
+  return ret;
 }
