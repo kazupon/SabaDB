ffd82a336cce27b2cca9cbbb1076667e046383c3
kazuya kawaguchi <kawakazu80@gmail.com>
2012-08-18 23:55:40 +0900

    update benchmark.

--- benchmark/roundtrips.c
+++ benchmark/roundtrips.c
@@ -16,8 +16,11 @@ typedef struct {
   uint64_t count;
 } echo_t;
 
+static uv_loop_t *loop;
 static int64_t TIME = 5000;
 static int64_t start_time;
+static uv_timer_t echo_timer;
+
 
 uv_buf_t on_alloc_buf(uv_handle_t *tcp, size_t size) {
   TRACE("tcp=%p, size=%zd\n", tcp, size);
@@ -27,6 +30,22 @@ uv_buf_t on_alloc_buf(uv_handle_t *tcp, size_t size) {
 void on_write(uv_write_t *req, int status) {
   TRACE("req=%p, status=%d\n", req, status);
   free(req);
+}
+
+void on_write_timer(uv_timer_t *timer, int status) {
+  TRACE("timer=%p, status=%d\n", timer, status);
+  echo_t *echo = (echo_t *)timer->data;
+
+  uv_buf_t buf;
+  buf.base = "hello\n";
+  buf.len = strlen(buf.base);
+  uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
+  if (uv_write(req, (uv_stream_t *)&echo->tcp, &buf, 1, on_write)) {
+    assert("uv_write failed\n");
+  }
+
+  uv_timer_stop(timer);
+  uv_close((uv_handle_t *)timer, NULL);
 }
 
 void on_read(uv_stream_t *tcp, ssize_t nread, uv_buf_t buf) {
@@ -39,14 +58,15 @@ void on_read(uv_stream_t *tcp, ssize_t nread, uv_buf_t buf) {
     return;
   }
 
-  if (!strcmp("hello", buf.base)) {
+  if (!strcmp("hello\n", buf.base)) {
     echo->count++;
     if (uv_now(tcp->loop) - start_time > TIME) {
       printf("ping-pong: %lld roundtrips/s\n", (1000 * echo->count) / TIME);
       uv_close((uv_handle_t *)tcp, NULL);
     } else {
       uv_buf_t buf;
-      buf.base = "hello";
+      //buf.base = "hello";
+      buf.base = "hello\n";
       buf.len = strlen(buf.base);
       uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
       if (uv_write(req, (uv_stream_t *)&echo->tcp, &buf, 1, on_write)) {
@@ -60,27 +80,27 @@ void on_connect(uv_connect_t *connection, int status) {
   TRACE("connection=%p, status=%d\n", connection, status);
   echo_t *echo = container_of(connection, echo_t, connection);
 
-  uv_buf_t buf;
-  buf.base = "hello";
-  buf.len = strlen(buf.base);
-  uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
-  if (uv_write(req, (uv_stream_t *)&echo->tcp, &buf, 1, on_write)) {
-    assert("uv_write failed\n");
-  }
-
   if (uv_read_start(connection->handle, on_alloc_buf, on_read)) {
     assert("uv_read_start failed\n");
   }
+
+  echo_timer.data = echo;
+  start_time = uv_now(loop);
+  uv_timer_init(loop, &echo_timer);
+  uv_timer_start(&echo_timer, on_write_timer, 1000, 5);
 }
 
 int main () {
-  uv_loop_t *loop = uv_default_loop();
-  start_time = uv_now(loop);
+  loop = uv_default_loop();
 
   echo_t echo;
   struct sockaddr_in client_addr = uv_ip4_addr("0.0.0.0", 0);
   struct sockaddr_in server_addr = uv_ip4_addr("127.0.0.1", 1978);
   uv_tcp_init(loop, &echo.tcp);
+  /*
+  uv_tcp_nodelay(&echo.tcp, 1);
+  uv_tcp_keepalive(&echo.tcp, 1, 60);
+  */
   uv_tcp_bind(&echo.tcp, client_addr);
   uv_tcp_connect(&echo.connection, &echo.tcp, server_addr, on_connect);
   echo.count = 0;
